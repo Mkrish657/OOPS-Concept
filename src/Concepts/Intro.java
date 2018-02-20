175b2bc02747720ea5c06ba297d7fcc83e488818
Muralikrishnan Sivakumar <mailtomkrish@gmail.com>
2018-02-20 08:47:55 +0530

    Intro coding done

---
+++ src/Concepts/Intro.java
@@ -0,0 +1,13 @@
+package Concepts;
+
+
+public class Intro {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Java");
+		
+			
+	}
+
+}
